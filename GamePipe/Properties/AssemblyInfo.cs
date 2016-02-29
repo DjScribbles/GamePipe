26e535e1610cd40d2e0e730379fb1e0b11f36c98
DjScribbles <joeshaw321@gmail.com>
2016-02-29 11:51:37 -0500

    Updated Version info

--- GamePipe/Properties/AssemblyInfo.cs
+++ GamePipe/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.2.*")]
+[assembly: AssemblyVersion("0.1.3.*")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
