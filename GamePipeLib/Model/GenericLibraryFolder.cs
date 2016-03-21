6f5a666a26b4220563dd51ff6acde356e82e29fa
DjScribbles <joeshaw321@gmail.com>
2016-03-21 13:53:04 -0400

    Added some initial work
    Added a skeleton GenericLibraryFolder
    Added JunctionPoint.cs from CodeProject

---
+++ GamePipeLib/Model/GenericLibraryFolder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePipeLib.Model
+{
+    class GenericGame
+    {
+        public string NativeDirectory;
+        public string ActualLocation;
+        public string Service;
+        public string GameDir;
+
+    }
+    class GenericLibraryFolder
+    {
+
+
+    }
+    class GameService
+    {
+        public string NativeDirectory;
+    }
+}
