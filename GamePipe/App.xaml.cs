45699e77f3a9fda4be1d9dba7b70a373760b1df1
DjScribbles <joeshaw321@gmail.com>
2016-12-06 17:07:49 -0500

    Added OctoKit and started checking for new versions
    Added OctoKit nuget package, using it at startup to check for new
    versions of Game Pipe on Github. If an update is available, a browser is
    opened to the latest release.

--- GamePipe/App.xaml.cs
+++ GamePipe/App.xaml.cs
@@ -5,6 +5,8 @@
 using System;
 using System.Windows;
 
+using System.Net.NetworkInformation;
+
 namespace GamePipe
 {
     /// <summary>
@@ -19,8 +21,8 @@ namespace GamePipe
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             this.Exit += App_Exit;
             var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-
             GamePipeLib.Utils.Logging.Logger.InfoFormat("-----------------------GamePipe started {0}-----------", version.ToString());
+            CheckForUpdates(version);
         }
 
         private void App_Exit(object sender, ExitEventArgs e)
@@ -37,6 +39,48 @@ namespace GamePipe
         private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             GamePipeLib.Utils.Logging.Logger.Error("An unhandled dispatcher exception occurred:", e.Exception);
+        }
+
+
+
+
+        public static async void CheckForUpdates(Version currentVersion)
+        {
+            try
+            {
+                var client = new Octokit.GitHubClient(new Octokit.ProductHeaderValue("GamePipe"));
+                var releases = client.Repository.Release.GetAll("DjScribbles", "GamePipe");
+                Version latestVersion = null;
+                Octokit.Release latestRelease = null;
+                foreach (var item in await releases)
+                {
+                    try
+                    {
+                        var thisVersion = new Version(item.TagName);
+                        if (thisVersion.CompareTo(latestVersion) > 0)
+                        {
+                            latestVersion = thisVersion;
+                            latestRelease = item;
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                if ((currentVersion.CompareTo(latestVersion) < 0) && (latestRelease != null))
+                {
+                    var result = MessageBox.Show("A new version of Game Pipe is available, do you want to take a look?", "New Version Available", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start(latestRelease.HtmlUrl);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                GamePipeLib.Utils.Logging.Logger.Error("Failed to check for updates due to exception:", ex);
+            }
         }
     }
 }
