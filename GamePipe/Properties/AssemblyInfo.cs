0759dcc8b893450dc65983f0c0be3f634b3836f1
DjScribbles <joeshaw321@gmail.com>
2022-04-27 09:13:09 -0400

    Release 1.0.5

--- GamePipe/Properties/AssemblyInfo.cs
+++ GamePipe/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.4.*")]
+[assembly: AssemblyVersion("1.0.5.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
